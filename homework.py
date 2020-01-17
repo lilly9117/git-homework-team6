e9300d85833d70e86a20d32b6aca765b1ab57f5f
hyebinchoi <lilly9117@naver.com>
2020-01-17 16:48:39 +0900

    add homework.py

---
+++ homework.py
@@ -0,0 +1,22 @@
+seconds = int(input("Enter the number of seconds: "))
+minutes = seconds / 60
+sec = seconds % 60
+sec2 = seconds % (60*60)
+sec3 = seconds % (60*60*60)
+hours = minutes / 60
+days = hours / 24
+              
+if seconds >= 86400 :
+    print(str(int(seconds)) + ' seconds are equal to:')
+    print(str(int(minutes)) + ' full minutes(s) and '+ str(int(sec)) + ' seconds.')
+    print(str(int(hours)) + ' full hours(s) and ' + str(int(sec2)) + ' seconds.')
+    print(str(int(days)) + ' full days(s) and ' + str(int(sec3)) + ' seconds.')
+elif seconds >= 3600:
+    print(str(int(seconds)) + ' seconds are equal to:')
+    print(str(int(minutes)) + ' full minutes(s) and '+ str(int(sec)) + ' seconds.')
+    print(str(int(hours)) + ' full hours(s) and ' + str(int(sec2)) + ' seconds.')          
+elif seconds >= 60:
+    print(str(int(seconds)) + ' seconds are equal to:')
+    print(str(int(minutes)) + ' full minutes(s) and '+ str(int(sec)) + ' seconds.')
+else:
+    print(str(int(seconds)) + ' seconds.')
